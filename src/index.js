71371706e6916031bd1e26b50b381581f97abe67
彭伟宏 <harry@pengweihongdeMacBook-Pro.local>
2017-01-06 01:11:53 +0800

    add react-router support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './components/Main';
+import About from './components/About';
+import Repos from './components/Repos';
 
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+// 使用路由时请用webpack-dev-server内联模式调试,即浏览器直接打开localhost:port
+// path="/" => localhost:port/#/
+// path="/about" => localhost:port/#/about
+// path="/repos" => localhost:port/#/repos
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
+    <Route path="/repos" component={Repos}/>
+  </Router>
+), document.getElementById('app'));
